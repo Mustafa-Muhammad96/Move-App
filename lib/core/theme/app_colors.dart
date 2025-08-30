fb707396352a18a2d0aa8cd7c1e3ebb45b160689
hussein shafeek <99825523+hussein-shafeek@users.noreply.github.com>
2025-08-30 15:00:25 +0300

    Initial commit of the Flutter project

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundWhite = Color(0xffFFFFFF);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff171717);
+  static const Color gray = Color(0xffA0A0A0);
+  static const Color backgroundDark = Color(0xff171717);
+}
