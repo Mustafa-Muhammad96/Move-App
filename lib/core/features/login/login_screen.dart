108e8650fa7c168b2fa34463d15ea407912498ec
Mostafa Muhammad <mostafa.ahmed@feng.bu.edu.eg>
2025-09-03 12:21:30 +0300

    update login screen

--- lib/core/features/login/login_screen.dart
+++ lib/core/features/login/login_screen.dart
@@ -1,11 +1,14 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/theme/app_colors.dart';
 import 'package:movie_app/core/utils/or_divider.dart';
 import 'package:movie_app/l10n/app_localizations.dart';
-//import 'package:movie_app/core/utils/default_elevated_button.dart';
 import 'package:movie_app/core/utils/default_text_form_field.dart';
-import '../../../widgets/default_elevated_button.dart';
+import 'package:movie_app/core/utils/default_elevated_button.dart';
+import 'package:provider/provider.dart';
+import '../../../providers/langauge_provider.dart';
 import '../../routes/routes.dart';
+import '../../utils/lang_toggle.dart';
 
 class LoginScreen extends StatelessWidget {
   static const String routeName = "/login";
@@ -13,83 +16,96 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final languageProvider = Provider.of<LanguageProvider>(context);
     return Scaffold(
       body: SafeArea(
           child: Padding(
             padding: EdgeInsets.all(12),
-            child: Column(
-             children: [
-               SizedBox(height: 40,),
-               Image.asset('assets/images/login_1.png'),
-               SizedBox(height: 20),
-               DefaultTextFormField(
-                 hintText: AppLocalizations.of(context)!.email,
-                 iconName: 'email',
-                 validator: (value) {
-                   if (value == null || value.length < 5) {
-                     return AppLocalizations.of(context)!.invalidEmail;
-                   }
-                   return null;
-                 },
-               ),
-               SizedBox(height: 20),
-               DefaultTextFormField(
-                 isPassword: true,
-                 hintText: AppLocalizations.of(context)!.password,
-                 iconName: 'password',
-                 validator: (value) {
-                   if (value == null || value.length < 8) {
-                     return AppLocalizations.of(
-                       context,
-                     )!
-                         .passwordMustBeAtLeast8Characters;
-                   }
-                   return null;
-                 },
-               ),
-               SizedBox(height: 20,),
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.end,
-                 children: [
-                   Text(
-                     AppLocalizations.of(context)!.forgetPassword,
-                     style: TextStyle(
-                       color: AppColors.yellow,
-                       fontSize: 14,
-                       fontWeight: FontWeight.w400
-                     ),
-                   )
-                 ],
-               ),
-               SizedBox(height: 30,),
-               DefaultElevatedButton(
-                 label: AppLocalizations.of(context)!.login,
-                 onPressed: () =>
-                     Navigator.pushNamed(context, AppRoutes.homeScreen),
-               ),
-               SizedBox(height: 20,),
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: [
-                   Text(AppLocalizations.of(context)!.notHaveAccount,
-                   style: TextStyle(
-                       color: AppColors.white,
-                       fontSize: 14,
-                       fontWeight: FontWeight.w400
-                   ),
-                   ),
-                   SizedBox(width: 6,),
-                   Text(AppLocalizations.of(context)!.createAccount,
-                     style: TextStyle(
+            child: SingleChildScrollView(
+              child: Column(
+               children: [
+                 SizedBox(height: 40,),
+                 Image.asset('assets/images/login_1.png'),
+                 SizedBox(height: 20),
+                 DefaultTextFormField(
+                   hintText: AppLocalizations.of(context)!.email,
+                   iconName: 'email',
+                   validator: (value) {
+                     if (value == null || value.length < 5) {
+                       return AppLocalizations.of(context)!.invalidEmail;
+                     }
+                     return null;
+                   },
+                 ),
+                 SizedBox(height: 20),
+                 DefaultTextFormField(
+                   isPassword: true,
+                   hintText: AppLocalizations.of(context)!.password,
+                   iconName: 'password',
+                   validator: (value) {
+                     if (value == null || value.length < 8) {
+                       return AppLocalizations.of(
+                         context,
+                       )!
+                           .passwordMustBeAtLeast8Characters;
+                     }
+                     return null;
+                   },
+                 ),
+                 SizedBox(height: 20,),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.end,
+                   children: [
+                     Text(
+                       AppLocalizations.of(context)!.forgetPassword,
+                       style: TextStyle(
                          color: AppColors.yellow,
                          fontSize: 14,
-                         fontWeight: FontWeight.w500
-                     ),),
-                 ],
-               ),
-               SizedBox(height: 20,),
-               OrDivider(),
-             ],
+                         fontWeight: FontWeight.w400
+                       ),
+                     )
+                   ],
+                 ),
+                 SizedBox(height: 30,),
+                 DefaultElevatedButton(
+                   label: AppLocalizations.of(context)!.login,
+                   onPressed: () =>
+                       Navigator.pushNamed(context, AppRoutes.homeScreen),
+                 ),
+                 SizedBox(height: 20,),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                     Text(AppLocalizations.of(context)!.notHaveAccount,
+                     style: TextStyle(
+                         color: AppColors.white,
+                         fontSize: 14,
+                         fontWeight: FontWeight.w400
+                     ),
+                     ),
+                     SizedBox(width: 6,),
+                     Text(AppLocalizations.of(context)!.createAccount,
+                       style: TextStyle(
+                           color: AppColors.yellow,
+                           fontSize: 14,
+                           fontWeight: FontWeight.w500
+                       ),),
+                   ],
+                 ),
+                 SizedBox(height: 20,),
+                 OrDivider(),
+                 SizedBox(height: 20,),
+                 DefaultElevatedButton(
+                   label: AppLocalizations.of(context)!.loginWithGoogle,
+                   onPressed:(){}
+                 ),
+                 SizedBox(height: 20,),
+                 LanguageSwitchRow(
+                   currentLang: languageProvider.locale.languageCode,
+                   onSelect: (lang) => languageProvider.changeLanguage(lang),
+                 ),
+               ],
+              ),
             ),
           )
       ),
