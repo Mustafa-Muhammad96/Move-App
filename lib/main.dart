8134ca7c380492a34ef92ddbfcfcc49ed9ba74d5
Mostafa Muhammad <mostafa.ahmed@feng.bu.edu.eg>
2025-09-03 12:19:08 +0300

    add some configs related to local

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:movie_app/core/features/login/login_screen.dart';
 import 'package:movie_app/core/features/register/register_screen.dart';
 import 'package:movie_app/core/theme/app_theme.dart';
 import 'package:movie_app/l10n/app_localizations.dart';
+import 'package:movie_app/providers/langauge_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
@@ -12,7 +14,11 @@ Future <void> main() async{
   final prefs=await SharedPreferences.getInstance();
   final seen= prefs.getBool("onboarding_seen")??false;
 
-  runApp( MyApp(seenOnboarding: seen));
+  runApp(ChangeNotifierProvider(
+      create: (_) => LanguageProvider(),
+      child: MyApp(seenOnboarding: seen)
+  )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,12 +28,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final languageProvider = Provider.of<LanguageProvider>(context);
     return MaterialApp(
         title: 'Movie-App',
         debugShowCheckedModeBanner: false,
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
-        locale: const Locale('ar'),
+        locale: languageProvider.locale,
         routes: {
           LoginScreen.routeName: (_) => LoginScreen(),
           RegisterScreen.routeName: (_) => RegisterScreen(),
