a65db6ba554d605c9257b2e1b97fddcb49486106
Mostafa Muhammad <mostafa.ahmed@feng.bu.edu.eg>
2025-09-09 01:49:29 +0300

    change initial route and create structure of folders in home feature

--- lib/core/routes/route_generator.dart
+++ lib/core/routes/route_generator.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movie_app/core/routes/routes.dart';
 
 import '../../features/forget_password/forget_password.dart';
-import '../../features/home/home_screen.dart';
+import '../../features/home/presentation/screens/home_screen.dart';
 import '../../features/login/presentation/screens/login_screen.dart';
 import '../../features/profile/ui/update_profile_screen.dart';
 import '../../features/register/register_screen.dart';
