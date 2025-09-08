797807d25748f90709f32b585e95aa37d45f9859
Mostafa Muhammad <mostafa.ahmed@feng.bu.edu.eg>
2025-09-09 01:28:37 +0300

    change initial route

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
       onGenerateRoute: RoutGenerator.getRoute,
       darkTheme: AppTheme.CustomeDarkTheme,
       themeMode: ThemeMode.dark,
-      initialRoute: AppRoutes.homeScreen,
+      initialRoute: AppRoutes.loginScreen,
       theme: AppTheme.CustomeLightTheme,
     );
   }
