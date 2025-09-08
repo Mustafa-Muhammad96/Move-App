ca25642fb25ccdd6638a4195955d6b6993443742
Yomna <yomnaomar197@gmail.com>
2025-09-09 01:05:09 +0400

    implement search tab UI

--- lib/tabs/search/search_tab.dart
+++ lib/tabs/search/search_tab.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/core/theme/app_colors.dart';
+import 'package:movie_app/core/widgets/default_text_form_field.dart';
+
 
 class SearchTab extends StatelessWidget {
   
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.white,
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              DefaultTextFormField(
+                iconName: "search",
+                hintText: "Search"
+                ),
+              
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Center(child: Image.asset("assets/images/search_empty.png"),)),
+                ),
+               
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
