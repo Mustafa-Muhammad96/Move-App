04e51019339c62ba3f70174ce48ca59099c3cf5b
hussein shafeek <99825523+hussein-shafeek@users.noreply.github.com>
2025-09-17 00:35:47 +0300

    Tap on any image navigates to MovieDetialsScreen; back button returns to previous page.

--- lib/tabs/browse/grid_view_items.dart
+++ lib/tabs/browse/grid_view_items.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/core/routes/routes.dart';
 
 class GridViewItems extends StatelessWidget {
- String imageName;
+  final String imageName;
 
- GridViewItems({required this.imageName});
+  GridViewItems({super.key, required this.imageName});
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset("assets/images/${imageName}.png" , width: double.infinity, fit: BoxFit.fill,);
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, AppRoutes.movieDetialsScreen);
+      },
+      child: Image.asset(
+        "assets/images/${imageName}.png",
+        width: double.infinity,
+        fit: BoxFit.fill,
+      ),
+    );
   }
 }
--- lib/tabs/home/action_items.dart
+++ lib/tabs/home/action_items.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_app/core/routes/routes.dart';
 
 class ActionItems extends StatelessWidget {
   final String imageUrl;
@@ -8,18 +9,23 @@ class ActionItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: CachedNetworkImage(
-        imageUrl: imageUrl,
-        fit: BoxFit.cover,
-        width: MediaQuery.sizeOf(context).width * 0.3,
-        height: MediaQuery.sizeOf(context).height * 0.25,
-        placeholder: (context, url) =>
-            Center(child: CircularProgressIndicator()),
-        errorWidget: (context, url, error) =>
-            Icon(Icons.broken_image, size: 50, color: Colors.grey),
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, AppRoutes.movieDetialsScreen);
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          fit: BoxFit.cover,
+          width: MediaQuery.sizeOf(context).width * 0.3,
+          height: MediaQuery.sizeOf(context).height * 0.25,
+          placeholder: (context, url) =>
+              const Center(child: CircularProgressIndicator()),
+          errorWidget: (context, url, error) =>
+              const Icon(Icons.broken_image, size: 50, color: Colors.grey),
+        ),
       ),
-      );
+    );
   }
 }
